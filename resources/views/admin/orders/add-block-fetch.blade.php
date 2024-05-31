9f6d8e51a3dba072148b3c2f1499b5288f60855c
Ahmed Basha <progahmedbasha08@gmail.com>
2024-05-31 05:24:15 +0300

    redisign some button

--- resources/views/admin/orders/add-block-fetch.blade.php
+++ resources/views/admin/orders/add-block-fetch.blade.php
@@ -18,12 +18,12 @@
         <tr>
             <td>{{ $index + 1 }} <input type="hidden" name="product_id[]" value="{{ $product->product_id }}" /></td>
             <td>{{ $product->product->name }}</td>
-            <td><input type="number" min="1" name="qty[]" data-id="{{ $product->id }}" style="width:60px" class="form-control qty" autocomplete="off" value="{{ $product->qty }}"></td>
+            <td><input type="number" min="1" name="qty[]" data-id="{{ $product->id }}" style="width:60px;     padding: 0.5rem 0rem;text-align: center;" class="form-control qty" autocomplete="off" value="{{ $product->qty }}"></td>
             <td> {{ $product->price }} <input type="hidden" class="item_price" value="{{ $product->price }}" /></td>
             <td class="price">{{ $product->total_cost }}</td>
             {{-- <td><input type="text" name="row_note[]" id="row_note" value="{{ $product->note }}" class="form-control" style="    width: 104px;"></td> --}}
             <td><input type="text" name="row_note[]" id="row_note_{{ $product->id }}" value="{{ $product->note }}" class="form-control note" style="width: 104px;"></td>
-            <td class="no-print"><button type="button" data-id="{{ $product->id }}" class="btn btn-danger delete_order_item delete" style="padding: 0px;"><i class="glyphicon glyphicon-trash"></i> <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
+            <td class="no-print"><button type="button" data-id="{{ $product->id }}" class="btn btn-danger delete_order_item delete" style="padding: 0px; margin-right: -17px;"><i class="glyphicon glyphicon-trash"></i> <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
                         <path d="M19.3248 9.46826C19.3248 9.46826 18.7818 16.2033 18.4668 19.0403C18.3168 20.3953 17.4798 21.1893 16.1088 21.2143C13.4998 21.2613 10.8878 21.2643 8.27979 21.2093C6.96079 21.1823 6.13779 20.3783 5.99079 19.0473C5.67379 16.1853 5.13379 9.46826 5.13379 9.46826" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                         <path d="M20.708 6.23975H3.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                         <path d="M17.4406 6.23973C16.6556 6.23973 15.9796 5.68473 15.8256 4.91573L15.5826 3.69973C15.4326 3.13873 14.9246 2.75073 14.3456 2.75073H10.1126C9.53358 2.75073 9.02558 3.13873 8.87558 3.69973L8.63258 4.91573C8.47858 5.68473 7.80258 6.23973 7.01758 6.23973" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
